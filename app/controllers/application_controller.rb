a6fb1eb279c39c768679aefe545870a7464f5b52
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-16 05:15:35 -0400

    Add splash page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+#     before_action :authenticate_user!
+
+#   private
+
+#   def before_sign_in_path_for(resource)
+#     # Redirect to the splash page after sign-in
+#     splash_path
+#   end
+
 end
