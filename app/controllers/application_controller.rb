f5701b77323ce2aa947a0da34f9ce03e91bc5d1b
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-16 05:26:24 -0400

    Add linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
-#     before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-#   private
+  protected
 
-#   def before_sign_in_path_for(resource)
-#     # Redirect to the splash page after sign-in
-#     splash_path
-#   end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name])
+  end
 end
