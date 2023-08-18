c4809f165a4aad7e0a68fad816549c39a065425b
Lul-Abdifan <tasanbannu@gmail.com>
2023-08-18 12:48:23 -0400

    Add unit testing for requests

---
+++ spec/requests/categories_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Categories", type: :request do
+  describe "GET /categories" do
+    before(:each)  do
+    user = User.create!(
+        full_name: 'Hope',
+        email: 'hope@example.com',
+        password: '1234567'
+      )
+    end
+
+    it "works! (now write some real specs)" do
+      post '/users/sign_in', params: { user: { email: 'hope@example.com', password: '1234567' } }
+   
+    
+      get categories_path
+      expect(response).to be_successful
+    end
+    
+    it "Should show Add Category" do
+      post '/users/sign_in', params: { user: { email: 'hope@example.com', password: '1234567' } }
+   
+    
+      get categories_path
+       expect(response.body).to include('Add Category')
+    end
+  end
+end
